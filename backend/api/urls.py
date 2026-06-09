274f1652d45863ab8cfd69bd8184de7389b2866c
Vihanga Anuththara <173596364+vanu888@users.noreply.github.com>
2026-06-09 21:24:17 +0530

    feat: add systemlogs option

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,10 @@
-from .views import predict_device_risk, get_device_risks
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     CompanyViewSet, DeviceViewSet, DeviceHealthLogViewSet, MaintenanceRecordViewSet, 
-    predict_device_risk, RegisterView, LoginView, ContractViewSet, TechnicianTaskViewSet, 
-    SystemUserViewSet, change_password, VerifyLoginOTPView, setup_2fa, verify_and_enable_2fa, disable_2fa
+    predict_device_risk, get_device_risks, RegisterView, LoginView, ContractViewSet, TechnicianTaskViewSet, 
+    SystemUserViewSet, change_password, VerifyLoginOTPView, setup_2fa, verify_and_enable_2fa, disable_2fa,
+    SystemLogAPIView, ClearSystemLogsAPIView
 )
 
 # API Router for CRUD operations
@@ -21,13 +21,20 @@ urlpatterns = [
     # Auth Links
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('change-password/', change_password, name='change_password'), # Password Change 
-    path('predict-risk/', predict_device_risk, name='predict-risk'),
-    path('device-risks/', get_device_risks, name='device-risks'),
-    path('', include(router.urls)), # CRUD API Links
-    path('login/', LoginView.as_view(), name='login'),
     path('login-verify-otp/', VerifyLoginOTPView.as_view(), name='login-verify-otp'),
+    path('change-password/', change_password, name='change_password'), 
     path('setup-2fa/', setup_2fa, name='setup-2fa'),
     path('verify-2fa/', verify_and_enable_2fa, name='verify-2fa'),
     path('disable-2fa/', disable_2fa, name='disable-2fa'),
+    
+    # ML Endpoint Links
+    path('predict-risk/', predict_device_risk, name='predict-risk'),
+    path('device-risks/', get_device_risks, name='device-risks'),
+    
+    # System Logs Links
+    path('system-logs/', SystemLogAPIView.as_view(), name='system-logs'),
+    path('system-logs/clear/', ClearSystemLogsAPIView.as_view(), name='clear-system-logs'),
+    
+    # CRUD API Links
+    path('', include(router.urls)), 
 ]
