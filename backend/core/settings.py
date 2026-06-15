219e08ee7f7e0e550d763959b31239798ad50f71
Vihanga Anuththara <173596364+vanu888@users.noreply.github.com>
2026-06-15 23:00:54 +0530

    feat: add email configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -115,3 +115,11 @@ REST_FRAMEWORK = {
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') # Fetches from .env
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # Fetches from .env
