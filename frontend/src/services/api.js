9dc348036b7cf035db5f7f9fc4ab7500f5907b03
Vihanga Anuththara <173596364+vanu888@users.noreply.github.com>
2026-06-17 22:46:20 +0530

    Fixed API_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api';
+const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api';
 
 const api = axios.create({
     baseURL: API_URL,
