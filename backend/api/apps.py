f18b76376733b4939efba9b5d75758ca9756f337
Vihanga Anuththara <173596364+vanu888@users.noreply.github.com>
2026-06-17 19:27:52 +0530

    feat: start scheduler at startup

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
-
+import os
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+    def ready(self):
+        if os.environ.get('RUN_MAIN') == 'true':
+            # Start scheduler at startup
+            from . import scheduler
+            scheduler.start()
