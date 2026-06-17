33b4e1dc0405e02875a095489873799fee1503bb
Vihanga Anuththara <173596364+vanu888@users.noreply.github.com>
2026-06-17 21:24:43 +0530

    feat: update cors_allowed_origins

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -107,6 +107,8 @@ STATIC_URL = 'static/'
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
