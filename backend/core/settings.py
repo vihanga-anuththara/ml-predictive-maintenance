1e4ef0ef19c9f171b738ed8f2bc2a2a0590961b0
Vihanga Anuththara <173596364+vihanga-anuththara@users.noreply.github.com>
2026-09-20 21:46:31 +0530

    chore: configure local development environment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0'] 
 
 # Application definition
 INSTALLED_APPS = [
@@ -133,9 +133,10 @@ STATIC_URL = 'static/'
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
     "http://localhost:5173",
+    "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
-    "https://ml-predictive-maintenance.vercel.app",
 ]
 
 REST_FRAMEWORK = {
